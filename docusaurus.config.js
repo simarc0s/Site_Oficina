d2957d112d8b98cfdb563dde160f7ea024cd9df1
Simão Marcos <a85489@ualg.pt>
2023-12-20 10:35:48 +0000

    alterei o titulo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Test Site',
+  title: 'Oficina_Automovel',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
