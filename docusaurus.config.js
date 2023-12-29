818d4de77525c8ee036cb3ba432aaf3db82b0b06
Gustavo Simes <a85879@ualg.pt>
2023-12-29 14:05:52 +0000

    corrigi o slogan

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Oficina Automóvel',
-  tagline: 'Nada se parte, Tudo se transforma',
+  tagline: 'Nada se perde, Tudo se transforma',
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
